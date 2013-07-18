4bdabb8d7cbf786aed687bfe54a1815a1cafeacb
Sam Dengler <sam.dengler@playonsports.com>
2013-07-18 06:39:36 -0400

    add mysql aliases

---
+++ mysql/aliases.zsh
@@ -0,0 +1,4 @@
+alias mysql-pr='mysql --defaults-extra-file=~/.mysql/playon-read.cnf -h wsdbread.playonsports.com bbs_owner'
+alias mysql-pw='mysql --defaults-extra-file=~/.mysql/playon-write.cnf -h wsdb.playonsports.com bbs_owner'
+alias mysql-sr='mysql --defaults-extra-file=~/.mysql/playon-read.cnf -h dbdev.playonsports.com bbs_owner'
+alias mysql-sw='mysql --defaults-extra-file=~/.mysql/playon-write.cnf -h dbdev.playonsports.com bbs_owner'
