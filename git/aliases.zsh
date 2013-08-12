4af069bcfe2f4ba771fe31228f751ffc343ca7cd
Sam Dengler <sam.dengler@playonsports.com>
2013-08-12 08:11:20 -0400

    add pl alias for gl typo.  add gpr alias for pull-request

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,8 +8,10 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias pl='gl'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push -u origin HEAD'
+alias gpr='git pull-request'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
